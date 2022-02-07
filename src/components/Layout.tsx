ef01d5173fd5981d484499708ea5162f504cb8b2
John Paulo Panganiban <olopangs@gmail.com>
2022-02-08 05:00:27 +0800

    folder and files structuring

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,13 @@
 import { FC } from "react";
 import styled from "styled-components";
 import { GlobalStyles } from "../styles/globals";
+import Header from "./Header";
 
 const Layout: FC = ({ children }) => {
   return (
     <>
       <GlobalStyles />
+      <Header/>
       <Container>{children}</Container>
     </>
   );
